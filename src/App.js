4f6a991c2cd77edab93d40f8e083fbe5804fe02f
Orxan905 <orxan.huseynli2002@mail.ru>
2022-12-07 11:34:01 +0400

    ready

--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import MovieList from './components/MovieList';
+import MovieListHeading from './components/MovieListHeading';
+import SearchBox from './components/SearchBox';
+import AddFavourites from './components/AddFavourites';
+import RemoveFavourites from './components/RemoveFavourites';
+import RemoveList from './components/RemoveList';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+	const [movies, setMovies] = useState([]);
+	const [searchValue, setSearchValue] = useState('');
+	const [favourites, setFavourites] = useState([]);
+
+	const getMovieRequest = async (searchValue) => {
+		const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=263d22d8`;
+
+		const response = await fetch(url);
+		const responseJson = await response.json();
+    
+		if (responseJson.Search) {
+			setMovies(responseJson.Search);
+		}
+	};
+
+	const addFavouriteMovie = (movie) => {
+		const newFavouriteList = [...favourites, movie];
+		setFavourites(newFavouriteList);
+	};
+
+	const removeFavouriteMovie = (movie) => {
+		const newFavouriteList = favourites.filter(
+			(favourite) => favourite.imdbID !== movie.imdbID
+		);
+
+		setFavourites(newFavouriteList);
+	};
+
+	useEffect(() => {
+		getMovieRequest(searchValue);
+	}, [searchValue]);
+
+	return (
+		<div className='container-fluid movie-app'>
+			<div className='row d-flex align-items-center'>
+				<MovieListHeading heading='MustSee' />
+			</div>
+      <div className='search'>
+        <div>
+        <p>Искать фильм по названию:</p>
+        <SearchBox searchValue={searchValue} setSearchValue={setSearchValue} />
+        </div>
+      </div>
+      <div className='remove'>
+        <input  placeholder='Enter list name' />
+				<RemoveList
+					movies={favourites}
+					handleFavouritesClick={removeFavouriteMovie}
+					favouriteComponent={RemoveFavourites}
+				/>
+        <button>Save List</button>
+			</div>
+			<div className='wrapper d-flex'>
+				<MovieList
+					movies={movies}
+					favouriteComponent={AddFavourites}
+					handleFavouritesClick={addFavouriteMovie}
+				/>
+			</div>
+		</div>
+	);
+};
 
 export default App;
---
+++ src/components/AddFavourites.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const AddFavourite = () => {
+	return (
+		<>
+			<span className='text-light'>Добавить в список</span>
+			
+		</>
+	);
+};
+
+export default AddFavourite;
